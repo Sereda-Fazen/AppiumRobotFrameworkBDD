4307a385117bf91120f4174bd646e58815d7d73a
Sereda-fazen <fazen7@mail.ru>
2017-12-20 15:38:00 +0200

    updated to 3 python

--- Browserstack.py
+++ Browserstack.py
@@ -35,7 +35,7 @@ class Browserstack(object):
         if cls.driver is None:
             cls.driver = webdriver.Remote('http://eugeneponomarenk1:R5AbyDrPiiBnt3pyaRUi@hub-cloud.browserstack.com'
                                           '/wd/hub',desired_caps_br)
-        return super(Browserstack, cls).__new__(cls, build, device)
+        return super().__new__(cls, build, device)
 
     def __init__(self, build, device):
         pass
